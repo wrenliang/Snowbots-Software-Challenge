cd108284d98438f3a9aed8b5a0c241136ffc864d
Wren Liang <wren.liang@gmail.com>
2019-09-10 21:25:39 -0700

    Initial Commit

---
+++ Snowbots v2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Snowbots v2
+//
+//  Created by Wren Liang on 2019-09-10.
+//  Copyright © 2019 Wren Liang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
